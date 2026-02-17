d083d0256798a0fe9678eee6d8d82ae9f3c21f04
liangyuwang <ly.wang.2022@outlook.com>
2026-02-17 23:57:44 +0300

    fix script bugs

--- scripts/debug_gpt_0.25b/pretrain.sh
+++ scripts/debug_gpt_0.25b/pretrain.sh
@@ -27,6 +27,7 @@ GBS=${GBS:-2097152}
 BATCH_SIZE=${BATCH_SIZE:-8}
 SEQ_LEN=${SEQ_LEN:-4096}
 SEP_SIZE=${SEP_SIZE:-1}
+USE_COMPILE=${USE_COMPILE:-1}
 
 echo "==================================="
 echo "Distributed Training Configuration"
@@ -67,8 +68,10 @@ TRAINING_ARGS="\
   --debug \
   --do_save \
   --save_every_steps 500 \
-  --use_compile \
 "
+if [ $USE_COMPILE -eq 1 ]; then
+  TRAINING_ARGS="$TRAINING_ARGS --use_compile"
+fi
 
 PARALLELISM_ARGS="\
   --sep_size $SEP_SIZE \
