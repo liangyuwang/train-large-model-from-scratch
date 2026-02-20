04a65924a3cb4e2809cb9f5bc3ff2976e9da363d
liangyuwang <ly.wang.2022@outlook.com>
2026-02-21 03:32:48 +0800

    fix all to all contiguous bug

--- distributed/expert_parallel/comm.py
+++ distributed/expert_parallel/comm.py
@@ -12,10 +12,10 @@ class EPAllToAll(torch.autograd.Function):
             (sum(output_splits), hidden_states.size(1)),
             dtype=hidden_states.dtype,
             device=hidden_states.device
-        )
+        ).contiguous()
         
         dist.all_to_all_single(
-            out_tensor, hidden_states,
+            out_tensor, hidden_states.contiguous(),
             output_split_sizes=output_splits,
             input_split_sizes=input_splits,
             group=ep_group
@@ -28,10 +28,10 @@ class EPAllToAll(torch.autograd.Function):
             (sum(ctx.input_splits), grad_output.size(1)),
             dtype=grad_output.dtype,
             device=grad_output.device
-        )
+        ).contiguous()
         
         dist.all_to_all_single(
-            grad_input, grad_output,
+            grad_input, grad_output.contiguous(),
             output_split_sizes=ctx.input_splits,
             input_split_sizes=ctx.output_splits,
             group=ctx.ep_group
